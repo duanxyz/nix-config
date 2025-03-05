47297c5b3becca74c7b6cb6e6bafa9815297fa0d
duan <duan@gmail.com>
2025-03-06 02:03:27 +0800

    feat(nixos-disko): move disko config to disko/semar

--- nix/nixos/hosts/semar/default.nix
+++ nix/nixos/hosts/semar/default.nix
@@ -11,9 +11,7 @@ in
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
-    ./disk-config.nix
-  ];
+    ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
