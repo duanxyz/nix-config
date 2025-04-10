46dd02359d035ec16df0b88b1cfe1e363cbef034
duan <duan@gmail.com>
2025-04-10 10:12:44 +0800

    feat(nixos-hardware): add entry boot limit

--- nix/nixos/hardwareProfiles/semar/default.nix
+++ nix/nixos/hardwareProfiles/semar/default.nix
@@ -13,6 +13,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
 
   boot.initrd.availableKernelModules = [
     "xhci_pci"
