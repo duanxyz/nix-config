37ab183e469dbc3e503054be2bede6c6cd9f7e1d
duan <duan@gmail.com>
2025-04-11 01:56:25 +0800

    feat(home-profiles): add full config hyprland in _config dir

---
+++ nix/home/homeProfiles/hyprland/_config/decoration.nix
@@ -0,0 +1,24 @@
+{
+  decoration = {
+    rounding = 8;
+
+    blur = {
+      enabled = true;
+      size = 6;
+      passes = 3;
+      new_optimizations = true;
+      noise = "0.01";
+      contrast = "1.2";
+      brightness = "1.1";
+      vibrancy = "0.5";
+      xray = true;
+    };
+
+    shadow = {
+      enabled = true;
+      range = 15;
+      render_power = 3;
+      scale = 1.05;
+    };
+  };
+}
---
+++ nix/home/homeProfiles/hyprland/_config/misc.nix
@@ -0,0 +1,13 @@
+{
+  misc = {
+    disable_hyprland_logo = true;
+    mouse_move_enables_dpms = true;
+    key_press_enables_dpms = true;
+
+    force_default_wallpaper = 0;
+    vfr = true;
+    vrr = 1;
+    enable_swallow = true;
+    swallow_regex = "^(kitty)$";
+  };
+}
