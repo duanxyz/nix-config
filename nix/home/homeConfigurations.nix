eb279d9c72a71fa7c743a0a3eba1e38656fdea58
duan <duan@gmail.com>
2025-04-10 21:36:47 +0800

    feat(home): add homeConfigurations

---
+++ nix/home/homeConfigurations.nix
@@ -0,0 +1,27 @@
+{
+  inputs,
+  cell,
+}:
+let
+  bee = {
+    system = "x86_64-linux";
+    home = inputs.home-manager;
+    pkgs = import inputs.nixpkgs {
+      inherit (inputs.nixpkgs) system;
+      config.allowUnfree = true;
+      overlays = [ ];
+    };
+  };
+
+  home = rec {
+    username = "duan";
+    homeDirectory = "/home/duan";
+    stateVersion = "24.11";
+  };
+in
+{
+  semar = {
+    inherit bee home;
+    imports = [ ];
+  };
+}
