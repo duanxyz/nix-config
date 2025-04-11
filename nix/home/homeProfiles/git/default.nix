c36976d3f051720f16294048f8c817318439c229
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2025-04-11 22:58:04 +0800

    feat(home-profiles/git): add extraConfig git

--- nix/home/homeProfiles/git/default.nix
+++ nix/home/homeProfiles/git/default.nix
@@ -7,5 +7,13 @@
     enable = true;
     userName = "Muhammad Ridwan";
     userEmail = "25993873+duanxyz@users.noreply.github.com";
+
+    extraConfig = {
+      core.editor = "hx";
+      core.autocrlf = "input";
+      pull.rebase = true;
+      init.defaultBranch = "main";
+      push.autoSetupRemote = true;
+    };
   };
 }
