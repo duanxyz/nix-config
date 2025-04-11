8275cd751a6d484c312fba175f077aa9f6824c4b
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2025-04-12 03:15:08 +0800

    feat(home-profiles/git): add signing key with ssh

--- nix/home/homeProfiles/git/default.nix
+++ nix/home/homeProfiles/git/default.nix
@@ -2,11 +2,21 @@
   inputs,
   cell,
 }:
+let
+  secrets = import (inputs.self + "/secrets/secrets.nix");
+  email = "25993873+duanxyz@users.noreply.github.com";
+in
 {
   programs.git = {
     enable = true;
     userName = "Muhammad Ridwan";
-    userEmail = "25993873+duanxyz@users.noreply.github.com";
+    userEmail = email;
+
+    signing = {
+      format = "ssh";
+      key = "~/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
 
     extraConfig = {
       core.editor = "hx";
@@ -14,6 +24,12 @@
       pull.rebase = true;
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
+
+      gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
     };
   };
+
+  home.file.".ssh/allowed_signers".text = ''
+    ${email} ssh-ed25519 ${secrets.userKey1}
+  '';
 }
