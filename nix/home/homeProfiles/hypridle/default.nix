eb6ab1f7ad52ffcfac2abc8fd113bbefa1e54e29
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2025-04-12 15:03:27 +0800

    feat(home-profiles/hypridle): add hypridle configurations

---
+++ nix/home/homeProfiles/hypridle/default.nix
@@ -0,0 +1,36 @@
+{
+  inputs,
+  cell,
+}:
+{
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+      };
+      listener = [
+        {
+          timeout = 150;
+          on-timeout = "brightnessctl -s set 10";
+          on-resume = "brightnessctl -r";
+        }
+        {
+          timeout = 300;
+          on-timeout = "loginctl lock-session";
+        }
+        {
+          timeout = 600;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on && brightnessctl -r";
+        }
+        {
+          timeout = 900;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+}
