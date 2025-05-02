d82f50f3f6ce2f170ecd629df6d2591565ec07c7
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2025-05-03 00:56:27 +0800

    refactor(secrets): update secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,18 +1,15 @@
 let
-  user1 = {
-    full = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHrizuun01yw/y8Rh6D5obIBhDrNvcCJI63gBdX6Vvr+ duan@semar";
-    key = "AAAAC3NzaC1lZDI1NTE5AAAAIHrizuun01yw/y8Rh6D5obIBhDrNvcCJI63gBdX6Vvr+";
-  };
+  user1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHrizuun01yw/y8Rh6D5obIBhDrNvcCJI63gBdX6Vvr+ duan@semar";
 
-  system1 = {
-    full = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFhPW9rORvLaWgrdOSnAxhTbTvJaaQV5I0SJA31hXPDJ root@semar";
-    key = "AAAAC3NzaC1lZDI1NTE5AAAAIFhPW9rORvLaWgrdOSnAxhTbTvJaaQV5I0SJA31hXPDJ";
-  };
+  system1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFhPW9rORvLaWgrdOSnAxhTbTvJaaQV5I0SJA31hXPDJ root@semar";
 in
 {
-  userKey1 = user1.key;
-  systemKey1 = system1.key;
-
-  "root.age".publicKeys = user1.full ++ system1;
-  "duan.age".publicKeys = user1.full ++ system1;
+  "root.age".publicKeys = [
+    user1
+    system1
+  ];
+  "duan.age".publicKeys = [
+    user1
+    system1
+  ];
 }
