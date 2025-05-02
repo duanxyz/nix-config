ef5dcbf6a7ddd0b6f5bd7a7c0206048a6ed1e54c
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2025-05-03 01:00:05 +0800

    refactor(disko/semar): refactor devices and partitions

--- nix/nixos/disko/semar/default.nix
+++ nix/nixos/disko/semar/default.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-id/nvme-PCIe_SSD_7EA8071717A400054997";
+        device = "/dev/disk/by-id/nvme-ADATA_LEGEND_900_2P012LA7C9H9";
         content = {
           type = "gpt";
           partitions = {
@@ -18,7 +18,7 @@
               };
             };
             root = {
-              size = "60G";
+              size = "100G";
               content = {
                 type = "filesystem";
                 format = "ext4";
