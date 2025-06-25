989675ed454c49673d2a89ac5c5e089f15031262
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2025-06-25 21:37:15 +0800

    feat(home-profiles/vscode): use nix-vscode-extensions
      - enable mutableExtensionsDir
      - use default profiles
      - add extensions

--- nix/home/homeProfiles/vscode/default.nix
+++ nix/home/homeProfiles/vscode/default.nix
@@ -6,5 +6,24 @@
   programs.vscode = {
     enable = true;
     package = pkgs.windsurf;
+    mutableExtensionsDir = true;
+    profiles = {
+      default = {
+        extensions =
+          with pkgs.open-vsx;
+          with pkgs.vscode-marketplace;
+          [
+            jnoortheen.nix-ide
+            eamodio.gitlens
+            aaron-bond.better-comments
+            jasew.vscode-helix-emulation
+            streetsidesoftware.code-spell-checker
+            streetsidesoftware.code-spell-checker-indonesian
+            hoovercj.vscode-settings-cycler
+            esbenp.prettier-vscode
+            tamasfe.even-better-toml
+          ];
+      };
+    };
   };
 }
