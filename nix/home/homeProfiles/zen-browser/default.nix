f53c57acc0fa84a804a28a1686a50ce4d0f2bdcb
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2025-09-13 14:39:40 +0800

    feat(home-profiles/zen-browser): set extensions default

--- nix/home/homeProfiles/zen-browser/default.nix
+++ nix/home/homeProfiles/zen-browser/default.nix
@@ -14,24 +14,47 @@
       isDefault = true;
     };
 
-    policies = {
-      AutofillAddressEnabled = true;
-      AutofillCreditCardEnabled = false;
-      DisableAppUpdate = true;
-      DisableFeedbackCommands = true;
-      DisableFirefoxStudies = true;
-      DisablePocket = true; # save webs for later reading
-      DisableTelemetry = true;
-      DontCheckDefaultBrowser = true;
-      # NoDefaultBookmarks = true;
-      OfferToSaveLogins = false;
-      EnableTrackingProtection = {
-        Value = true;
-        Locked = true;
-        Cryptomining = true;
-        Fingerprinting = true;
+    policies =
+      let
+        mkExtensionSettings = builtins.mapAttrs (
+          _: pluginId: {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/${pluginId}/latest.xpi";
+            installation_mode = "force_installed";
+          }
+        );
+
+        mkLockedAttrs = builtins.mapAttrs (
+          _: value: {
+            Value = value;
+            Status = "locked";
+          }
+        );
+      in
+      {
+        ExtensionSettings = mkExtensionSettings {
+          "{036a55b4-5e72-4d05-a06c-cba2dfcc134a}" = "traduzir-paginas-web";
+          "uBlock0@raymondhill.net" = "ublock-origin";
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = "bitwarden-password-manager";
+          "tridactyl.vim@cmcaine.co.uk" = "tridactyl-vim";
+        };
+
+        AutofillAddressEnabled = true;
+        AutofillCreditCardEnabled = false;
+        DisableAppUpdate = true;
+        DisableFeedbackCommands = true;
+        DisableFirefoxStudies = true;
+        DisablePocket = true; # save webs for later reading
+        DisableTelemetry = true;
+        DontCheckDefaultBrowser = true;
+        # NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        # find more options here: https://mozilla.github.io/policy-templates/
       };
-      # find more options here: https://mozilla.github.io/policy-templates/
-    };
   };
 }
