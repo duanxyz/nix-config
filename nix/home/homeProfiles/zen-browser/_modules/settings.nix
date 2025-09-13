e9e0f52a56e38ecd7afe87e1ed267aa02cf55890
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2025-09-13 14:51:24 +0800

    feat(home-profiles/zen-browser): set zen custom settings in default profile

---
+++ nix/home/homeProfiles/zen-browser/_modules/settings.nix
@@ -0,0 +1,9 @@
+{
+  programs.zen-browser.profiles."default".settings = {
+    "zen.view.experimental-rounded-view" = true;
+    "zen.widget.linux.transparency" = true;
+    "devtools.chrome.enabled" = true;
+    "devtools.debugger.remote-enabled" = true;
+    "view_source.wrap_long_lines" = true;
+  };
+}
