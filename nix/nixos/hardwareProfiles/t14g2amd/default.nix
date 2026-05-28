0bd35774714b7d295f0e69c3b8e6d67412e9573b
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2026-05-28 18:27:56 +0000

    refactor(t14g2amd): move machine settings into hardware profile

--- nix/nixos/hardwareProfiles/t14g2amd/default.nix
+++ nix/nixos/hardwareProfiles/t14g2amd/default.nix
@@ -1,12 +1,16 @@
 {
   config,
   lib,
+  pkgs,
+  inputs,
+  cell,
   ...
 }:
 {
   imports = [
     inputs.disko.nixosModules.default
     cell.diskoConfigurations.t14g2amd
+    inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen2
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
 
@@ -21,6 +25,14 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  # NOTE: Keep bootloader and kernel choice with this machine-specific profile.
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 10;
+  };
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+
   swapDevices = [ ];
 
   zramSwap = {
@@ -34,6 +46,14 @@
   };
 
   services.fstrim.enable = true;
+  services.fwupd.enable = true;
+
+  hardware.firmware = [ pkgs.linux-firmware ];
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
--- nix/nixos/hosts/t14g2amd/default.nix
+++ nix/nixos/hosts/t14g2amd/default.nix
@@ -1,7 +1,6 @@
 {
   inputs,
   cell,
-  pkgs,
   ...
 }:
 let
@@ -17,17 +16,8 @@ in
   ++ (with cell.nixosSuites; [
     workstation
   ]);
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "t14g2amd";
-
-  # Configure network connections interactively with nmcli or nmtui.
-  networking.networkmanager.enable = true;
 
   system.stateVersion = "25.11";
 }
